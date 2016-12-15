d639bc09f38ecd799a022a3b07ba1d0c7e6f3352
wanpeng16 <wanpengwx@163.com>
2016-12-15 22:05:02 +0800

    查找目标元素边界

---
+++ 查找元素边界.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+#-*-coding=utf-8-*-
+class Solution(object):
+    def searchRange(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        def findrightpos(num,target,lo,hi):
+            while lo < hi:
+                mid= (lo+hi) // 2
+                if target < num[mid]: #[lo,mid) (mid,hi)
+                    hi = mid
+                else:
+                    lo = mid + 1
+            return lo
+        res=[-1,-1]
+        leftpos=findrightpos(nums,target-1,0,len(nums))
+        if leftpos==len(nums) or nums[leftpos]!=target:
+            return res
+        else:
+            rightpos=findrightpos(nums,target,0,len(nums))
+            return [leftpos,rightpos-1]
